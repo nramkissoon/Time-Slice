2db14ae299f8a1de975a616574928efcdfa60398
Nicholas Ramkissoon <nr1610@nyu.edu>
2019-08-30 18:34:18 -0400

    Added reverse functionality to cli

--- timeslicer/__main__.py
+++ timeslicer/__main__.py
@@ -4,9 +4,19 @@ print("Timeslice:")
 direc = input("Enter directory path to photos: ")
 output = input("Enter path for output timeslicer (eg. directory_name/photo.jpg): ")
 s = int(input("Enter skip value (how many photos to skip over when selecting slices): "))
+
+r = str(input("Reverse (y/n)?"))
+while r.lower() not in ['y', 'n']:
+    r = input("Reverse (y/n)?")
+
+rev = True
+if r == 'n':
+    rev = False
+
 q = int(input("Enter output quality (1 - 100): "))
+
 
 print("")
 print("Creating timeslicer...")
-timeslice(direc, output_path=output, skip=s, qual=q)
+timeslice(direc, output_path=output, skip=s, reverse=rev, qual=q)
 print("Finished.")
