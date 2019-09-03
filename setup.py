cce9f12fb6aac73b74fba478ee07d2b34e9098d5
Nicholas Ramkissoon <nr1610@nyu.edu>
2019-09-03 16:50:13 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="timeslicer",
-    version="1.2.0",
+    version="1.3.0",
     author="Nicholas Ramkissoon",
     description="Package for creating timeslice images from time lapses",
     long_description=long_description,
